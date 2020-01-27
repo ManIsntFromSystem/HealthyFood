99412dd9133d13ad4de6cf636ee53768f281e6b3
dimuch1029 <dimuch.logan@gmail.com>
2020-01-27 17:11:09 +0300

    Optimise images and methods

--- app/src/main/java/com/example/sportthings/SportThingsMain.java
+++ app/src/main/java/com/example/sportthings/SportThingsMain.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -16,8 +17,10 @@ import butterknife.ButterKnife;
 
 public class SportThingsMain extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @BindView(R.id.textPrice) TextView txtTotalPrice;
-    @BindView(R.id.textPrice) TextView textViewQuantity;
-    Spinner spinner;
+    @BindView(R.id.textQuantity) TextView textViewQuantity;
+    @BindView(R.id.imageViewProduct) ImageView imageViewProduct;
+    @BindView(R.id.spinner) Spinner spinner;
+
     HashMap<String, Double> listFoodHashMap;
     private static int quantityProduct = 1;
     String productName;
@@ -29,30 +32,15 @@ public class SportThingsMain extends AppCompatActivity implements AdapterView.On
         setContentView(R.layout.sport_things_main);
         ButterKnife.bind(this);
 
-        spinner = findViewById(R.id.spinner);
-        spinner.setOnItemSelectedListener(this);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.food_list, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
-        listFoodHashMap = new HashMap<>();
-        listFoodHashMap.put("Broccoli", 1.2);
-        listFoodHashMap.put("Tomato", 0.7);
-        listFoodHashMap.put("Cucumber", 0.5);
-        listFoodHashMap.put("Salad", 1.0);
-        listFoodHashMap.put("Avocado", 3.0);
-        listFoodHashMap.put("White cabbage", 0.9);
-        listFoodHashMap.put("Chickpeas", 1.1);
+        createSpinner();
+        createProductMap();
+        setDate();
     }
 
     public void incQuantity(View view) {
         try {
             quantityProduct++;
-            textViewQuantity.setText("" + quantityProduct);
-            productName = spinner.getSelectedItem().toString();
-            productPrice = listFoodHashMap.get(productName);
-            txtTotalPrice.setText("" + (productPrice * quantityProduct));
+            setDate();
         } catch (Exception e){
             System.out.println(e);
         }
@@ -62,10 +50,7 @@ public class SportThingsMain extends AppCompatActivity implements AdapterView.On
         try {
             if(quantityProduct > 1){
                 quantityProduct--;
-                textViewQuantity.setText("" + quantityProduct);
-                productName = spinner.getSelectedItem().toString();
-                productPrice = listFoodHashMap.get(productName);
-                txtTotalPrice.setText("" + (productPrice * quantityProduct));
+                setDate();
             }
         } catch (Exception e){
             System.out.println(e);
@@ -75,14 +60,61 @@ public class SportThingsMain extends AppCompatActivity implements AdapterView.On
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         quantityProduct = 1;
-
-        productName = spinner.getSelectedItem().toString();
-        productPrice = listFoodHashMap.get(productName);
-        txtTotalPrice.setText("" + (productPrice * quantityProduct));
+        setDate();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+    }
 
+    public void setDate() {
+        textViewQuantity.setText("" + quantityProduct);
+
+        productName = spinner.getSelectedItem().toString();
+        productPrice = listFoodHashMap.get(productName);
+        txtTotalPrice.setText("" + (productPrice * quantityProduct));
+
+        switch (productName) {
+            case "Broccoli":
+                imageViewProduct.setImageResource(R.drawable.broccoli1);
+                break;
+            case "Tomato":
+                imageViewProduct.setImageResource(R.drawable.tomato1);
+                break;
+            case "Cucumber":
+                imageViewProduct.setImageResource(R.drawable.cucumber1);
+                break;
+            case "Salad":
+                imageViewProduct.setImageResource(R.drawable.salad1);
+                break;
+            case "Avocado":
+                imageViewProduct.setImageResource(R.drawable.avocado1);
+                break;
+            case "White cabbage":
+                imageViewProduct.setImageResource(R.drawable.cabbage1);
+                break;
+            case "Chickpeas":
+                imageViewProduct.setImageResource(R.drawable.chickpeas1);
+                break;
+        }
+    }
+
+    public void createSpinner(){
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.food_list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void createProductMap(){
+        listFoodHashMap = new HashMap<>();
+        listFoodHashMap.put("Broccoli", 1.2);
+        listFoodHashMap.put("Tomato", 0.7);
+        listFoodHashMap.put("Cucumber", 0.5);
+        listFoodHashMap.put("Salad", 1.0);
+        listFoodHashMap.put("Avocado", 3.0);
+        listFoodHashMap.put("White cabbage", 0.9);
+        listFoodHashMap.put("Chickpeas", 1.1);
     }
 }
